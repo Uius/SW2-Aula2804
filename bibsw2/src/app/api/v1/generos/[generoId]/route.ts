1b5655e946708ebe7ac588c6eb364bf07804abd7
William <168018978+Uius@users.noreply.github.com>
2025-05-05 11:37:21 +0000

    Aula 05/05/2025

---
+++ bibsw2/src/app/api/v1/generos/[generoId]/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+import { isContext } from "vm";
+
+export async function GET(request: Request, context : any) {
+    const {params} = context;
+
+    return NextResponse.json({"param" : params.generoId})
+}
