1f89789c52237120df7467458838aa506dd0518f
William <168018978+Uius@users.noreply.github.com>
2025-05-05 10:53:21 +0000

    Atualização TEste

--- bibsw2/src/app/api/v1/generos/route.ts
+++ bibsw2/src/app/api/v1/generos/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 
+import dados from "@/db.json"
+
 export async function GET() {
-    return NextResponse.json({ "mensagem" : "Generos"});
+    //return NextResponse.json({ "mensagem" : "Generos"});
+    return NextResponse.json(dados);
 }
