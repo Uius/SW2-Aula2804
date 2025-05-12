093161475c469d9b8bc561cb31a1e599caebfbde
William <168018978+Uius@users.noreply.github.com>
2025-05-12 10:44:40 +0000

    isso

--- bibsw2/src/app/api/v1/generos/[generoId]/route.ts
+++ bibsw2/src/app/api/v1/generos/[generoId]/route.ts
@@ -4,5 +4,5 @@ import { isContext } from "vm";
 export async function GET(request: Request, context : any) {
     const {params} = context;
 
-    return NextResponse.json({"param" : params.generoId})
+    return NextResponse.json({"param" : context.generoId})
 }
